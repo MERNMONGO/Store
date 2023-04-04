34acdac37d31fde49635e177492549b7997da29e
MERNMONGO <127901439+MERNMONGO@users.noreply.github.com>
2023-04-04 15:09:34 -0400

    cleaning code

--- capp/client/src/App.js
+++ capp/client/src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import { userInfo } from './services/userService';
 
